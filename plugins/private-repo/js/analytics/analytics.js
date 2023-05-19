12c6de70393239a2761aa40319b8081f148fe061
tporret <tporret@ncsu.edu>
2023-05-19 08:54:04 -0400

    partytime ga4

--- plugins/private-repo/js/analytics/analytics.js
+++ plugins/private-repo/js/analytics/analytics.js
@@ -1,6 +1,11 @@
 // Version: 1.0.0
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
 
+function gtag() {
+    dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
 gtag('config', 'G-C1N082Q0S4');
+
+window.dispatchEvent(new CustomEvent('ptupdate'));
--- plugins/private-repo/private-repo.php
+++ plugins/private-repo/private-repo.php
@@ -27,16 +27,17 @@ private_repo_install_plugins_table_api_args();
 
 // Enqueue js/partytowm/lib/partytown.js
 function private_repo_enqueue_scripts() {
-    wp_enqueue_script( 'partytime', plugins_url( 'js/partytown/lib/partytown.js', __FILE__ ), array(), '1.0.0', false );
-    wp_enqueue_script( 'ga4', 'https://www.googletagmanager.com/gtag/js?id=G-C1N082Q0S4', array(), '1.0.0', true );
-    wp_enqueue_script( 'google-analytics', plugins_url( 'js/analytics/analytics.js', __FILE__ ), array(), '1.0.0', true );
+    wp_enqueue_script( 'partytown', plugins_url( 'js/partytown/lib/partytown.js', __FILE__ ), array(), '1.0.0', false );
+    wp_enqueue_script( 'ga4', 'https://www.googletagmanager.com/gtag/js?id=G-C1N082Q0S4', array('partytown'), '1.0.0', true );
+    wp_enqueue_script( 'google-analytics', plugins_url( 'js/analytics/analytics.js', __FILE__ ), array('partytown'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'private_repo_enqueue_scripts' );
 
 // Party Time !!!
 function add_type_to_script( $tag, $handle, $src ) {
-    if ( 'ga4' === $handle || 'google-analytics' === $handle ) {
-        $tag = '<script type="text/partytown" src="' . esc_url( $src ) . '"></script>';
+
+    if ( $handle === 'ga4' || $handle === 'google-analytics' ) {
+        $tag = '<script type="text/partytown" src="' . esc_url( $src ) . '" class="' . $handle . '-pt"></script>';
     }
 
     return $tag;
